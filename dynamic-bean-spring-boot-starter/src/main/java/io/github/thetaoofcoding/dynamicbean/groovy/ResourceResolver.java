669347b08e80a3cc0a58ed6633dbc1484a1fcbce
zailushang <631271779@qq.com>
2026-05-06 12:53:18 +0800

    登山不以艰险而止，则必臻乎峻岭矣。

--- dynamic-bean-spring-boot-starter/src/main/java/io/github/thetaoofcoding/dynamicbean/groovy/ResourceResolver.java
+++ dynamic-bean-spring-boot-starter/src/main/java/io/github/thetaoofcoding/dynamicbean/groovy/ResourceResolver.java
@@ -3,6 +3,7 @@ package io.github.thetaoofcoding.dynamicbean.groovy;
 import io.github.thetaoofcoding.dynamicbean.core.SAM;
 import io.github.thetaoofcoding.dynamicbean.model.RefreshableBeanModel;
 import io.github.thetaoofcoding.dynamicbean.scope.RefreshableScope;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.core.env.Environment;
@@ -13,15 +14,20 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 public interface ResourceResolver<T, R> {
     R resolve(T t);
 
+    @Slf4j
     class ResourceResolvers {
         public static ResourceResolver<RefreshableBeanModel, BeanDefinitionHolder> beanDefinitionResolver(GroovyShellFactory groovyShellFactory) {
             return refreshableBeanModel -> {
                 var script = refreshableBeanModel.script();
+                // 将 Groovy 脚本解析为 SAM 实例
                 var sam = (SAM<?, ?>) groovyShellFactory.create()
                         .evaluate(script);
+
                 var beanName = refreshableBeanModel.beanName();
-                var beanDefinition = BeanDefinitionBuilder
-                        .genericBeanDefinition(SAM.class, () -> sam)
+                log.debug("resolve dynamic bean : '{}'", beanName);
+
+                // 构造 BeanDefinition
+                var beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(SAM.class, () -> sam)
                         .setScope(RefreshableScope.SCOPE_NAME)
                         .getBeanDefinition();
                 return new BeanDefinitionHolder(beanDefinition, beanName);
@@ -30,6 +36,7 @@ public interface ResourceResolver<T, R> {
 
         public static ResourceResolver<Environment, JdbcTemplate> earlyJdbcTemplateResolver() {
             return environment -> {
+                log.debug("resolve early jdbcTemplate...");
                 var url = environment.getProperty("spring.datasource.url");
                 var username = environment.getProperty("spring.datasource.username");
                 var password = environment.getProperty("spring.datasource.password");
--- dynamic-schedule-spring-boot-starter/src/main/java/io/github/thetaoofcoding/dynamicschedule/service/impl/ScheduledTaskRegistrar.java
+++ dynamic-schedule-spring-boot-starter/src/main/java/io/github/thetaoofcoding/dynamicschedule/service/impl/ScheduledTaskRegistrar.java
@@ -3,6 +3,7 @@ package io.github.thetaoofcoding.dynamicschedule.service.impl;
 import io.github.thetaoofcoding.dynamicbean.core.SAM;
 import io.github.thetaoofcoding.dynamicschedule.model.ScheduledTaskDefinition;
 import io.github.thetaoofcoding.dynamicschedule.service.ScheduledTaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -11,15 +12,19 @@ import org.springframework.scheduling.support.CronTrigger;
 import java.util.Map;
 import java.util.concurrent.ScheduledFuture;
 
+@Slf4j
 public record ScheduledTaskRegistrar(
         ThreadPoolTaskScheduler threadPoolTaskScheduler,
         @Qualifier("scheduledFutureMap") Map<String, ScheduledFuture<?>> scheduledFutureMap,
         ApplicationContext applicationContext) implements ScheduledTaskService {
+
     @Override
     public void register(ScheduledTaskDefinition scheduledTaskDefinition) {
         var beanName = scheduledTaskDefinition.beanName();
         var cronExpression = scheduledTaskDefinition.cronExpression();
         var registryKey = scheduledTaskDefinition.registryKey();
+
+        log.debug("register dynamic scheduled task : '{}'", registryKey);
 
         scheduledFutureMap.computeIfAbsent(registryKey, _ -> {
             // 查找动态任务（SAM 类型）
@@ -31,6 +36,7 @@ public record ScheduledTaskRegistrar(
 
     @Override
     public void unregister(String registryKey) {
+        log.debug("unregister dynamic scheduled task : '{}'", registryKey);
         scheduledFutureMap.computeIfPresent(registryKey, (_, v) -> {
             // 取消定时任务
             v.cancel(true);
@@ -41,6 +47,7 @@ public record ScheduledTaskRegistrar(
 
     @Override
     public void reregister(ScheduledTaskDefinition scheduledTaskDefinition) {
+        log.debug("reregister dynamic scheduled task : '{}'", scheduledTaskDefinition.registryKey());
         synchronized (scheduledFutureMap) {
             // 取消并移除
             unregister(scheduledTaskDefinition.registryKey());
